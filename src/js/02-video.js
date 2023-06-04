5ea37cd233de7508472610227820b22c20d81edc
OlenaPavlovska <100314322+OlenaPavlovska@users.noreply.github.com>
2023-06-04 16:24:21 +0300

    task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle  from "lodash.throttle";
+import Player from '@vimeo/player';
+const iframe = document.getElementById("vimeo-player")
+const player = new Player(iframe)
 
+function playerTime(event) {
+    const eventTime = event.seconds;
+    localStorage.setItem("videoplayer-current-time", eventTime);
+    
+};
+
+player.on('timeupdate', throttle(playerTime, 1000));
+window.addEventListener('DOMContentLoaded', setTime);
+
+
+function setTime() {
+    const getStorage = localStorage.getItem("videoplayer-current-time");
+    if (getStorage) {
+        player.setCurrentTime(getStorage)
+    }
+}
